5063fdc8e7b8384998c125041ebe7cd6d80806f3
Jeff Soo <jeff@ipsedixit.net>
2017-03-11 18:04:06 -0500

    Version 0.1.2: documentation tidy (and new compile workflow)

---
+++ config-dist.php
@@ -0,0 +1,26 @@
+<?php
+
+// Compiler configuration for a Textpattern plugin source file.
+// 
+// Required: uncomment the 'path' line and add the correct path to the 
+// plugin compiler.
+// 
+// Optionally, uncomment the 'helpfile' line and add the relative path 
+// to a separate help file.
+//
+// Also optionally, uncomment the appropriate 'parser' line.
+// You will have to install the parser yourself. The compiler associated
+// with this config file currently expects either PHP-Textile or Parsedown.
+// Then provide the path to the parser(s) in the config file for zem_tpl.php.
+// (NB: Parsedown is a parser for GitHub-flavoured Markdown.)
+// https://github.com/erusev/parsedown
+// https://github.com/textile/php-textile
+
+$compiler_cfg = array(
+#     'path'     => 'path/to/textpattern-plugin-template/zem_tpl.php',
+#     'helpfile' => 'README.md',
+#     'parser'   => 'textile',
+#     'parser'   => 'parsedown',
+);
+
+?>
--- soo_hide.php
+++ soo_hide.php
@@ -1,100 +1,39 @@
 <?php
-$plugin['name'] = 'soo_hide';
-$plugin['version'] = '0.1.0';
+$plugin['version'] = '0.1.2';
 $plugin['author'] = 'Jeff Soo';
 $plugin['author_uri'] = 'http://ipsedixit.net/';
 $plugin['description'] = 'Replacement for txp:hide';
 $plugin['type'] = 0; 
 
+// Allow raw HTML help, as opposed to Textile.
+// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
+// 1 = Plugin help is in raw HTML.  Not recommended.
+$plugin['allow_html_help'] = 1;
+
 defined('txpinterface') or @include_once('zem_tpl.php');
 
+if (! defined('txpinterface')) {
+    global $compiler_cfg;
+    @include_once('config.php');
+    @include_once($compiler_cfg['path']);
+}
 # --- BEGIN PLUGIN CODE ---
 
-function soo_hide ( $atts, $thing )
+function soo_hide ($atts, $thing)
 {
-	extract(lAtts(array(
- 		'force' => 0,
-	), $atts));
-	
-	global $production_status;
-	if ( ! $force ) switch ( $production_status )
-	{
-		case 'debug': return parse($thing);
-		case 'testing': return '<!-- ' . parse($thing) . ' -->';
-	}
-	return '';
+    extract(lAtts(array(
+        'force' => 0,
+    ), $atts));
+    
+    global $production_status;
+    if (! $force) switch ($production_status) {
+        case 'debug': return parse($thing);
+        case 'testing': return '<!-- '.parse($thing).' -->';
+    }
+    
+    return '';
 }
 
 # --- END PLUGIN CODE ---
-
-if (0) {
-?>
-<!-- CSS SECTION
-# --- BEGIN PLUGIN CSS ---
-<style type="text/css">
-div#sed_help pre {padding: 0.5em 1em; background: #eee; border: 1px dashed #ccc;}
-div#sed_help h1, div#sed_help h2, div#sed_help h3, div#sed_help h3 code {font-family: sans-serif; font-weight: bold;}
-div#sed_help h1, div#sed_help h2, div#sed_help h3 {margin-left: -1em;}
-div#sed_help h2, div#sed_help h3 {margin-top: 2em;}
-div#sed_help h1 {font-size: 2.4em;}
-div#sed_help h2 {font-size: 1.8em;}
-div#sed_help h3 {font-size: 1.4em;}
-div#sed_help h4 {font-size: 1.2em;}
-div#sed_help h5 {font-size: 1em;margin-left:1em;font-style:oblique;}
-div#sed_help h6 {font-size: 1em;margin-left:2em;font-style:oblique;}
-div#sed_help li {list-style-type: disc;}
-div#sed_help li li {list-style-type: circle;}
-div#sed_help li li li {list-style-type: square;}
-div#sed_help li a code {font-weight: normal;}
-div#sed_help li code:first-child {background: #ddd;padding:0 .3em;margin-left:-.3em;}
-div#sed_help li li code:first-child {background:none;padding:0;margin-left:0;}
-div#sed_help dfn {font-weight:bold;font-style:oblique;}
-div#sed_help .required, div#sed_help .warning {color:red;}
-div#sed_help .default {color:green;}
-div#sed_help dt {font-weight: bold;}
-div#sed_help dd {margin: 0.5em 2em;}
-div#sed_help dd + dt {margin-top: 1em;}
-</style>
-# --- END PLUGIN CSS ---
--->
-<!-- HELP SECTION
-# --- BEGIN PLUGIN HELP ---
- <div id="sed_help">
-
-h1. soo_hide
-
-Drop-in replacement for the core " @hide@ ":http://textpattern.net/wiki/index.php?title=hide tag, offering different behavior based on the site's "Production Status":http://textpattern.net/wiki/index.php?title=Basic_Preferences#Production_Status setting:
-
-; Live
-: Hide tag contents
-; Testing
-: Output tag contents, enclosed by HTML comment delimiters
-; Debugging
-: Display tag contents
-
-To force the tag to hide its contents even when in Testing or Debugging mode, set the tag's @force@ attribute to @1@.
-
-h2(#usage). Usage
-
-pre.. <txp:soo_hide>
-	This will run when the site is in Debugging or Testing mode 
-	(in Testing mode it will be enclosed in HTML comments).
-</txp:soo_hide>
-
-<txp:soo_hide force="1">Your secret is safe with me.</txp:soo_hide>
-
-h2(#history). Version History
-
-h3. 0.1.0 (2011/01/13)
-
-* Initial release. 
-* Behavior based on site production status.
-* Override test/debug behavior with @force@ attribute.
-
- </div>
-# --- END PLUGIN HELP ---
--->
-<?php
-}
 
 ?>
